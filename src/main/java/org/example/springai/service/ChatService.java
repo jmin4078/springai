66d704d551aec9877c4aae55edbf74956a38390e
jmin4078 <wpalsrl8588@gmail.com>
2026-07-28 11:09:17 +0900

    Add Gemini chat configuration

--- src/main/java/org/example/springai/service/ChatService.java
+++ src/main/java/org/example/springai/service/ChatService.java
@@ -13,7 +13,11 @@ public class ChatService {
     @Qualifier("openAiChatModel") // Bean 이름으로 타입 기반 의존성 주입된 것 중 필요한 걸 지정
     private final ChatModel chatModel;
 
+    @Qualifier("googleGenAiChatModel")
+    private final ChatModel geminiChatModel;
+
     public String chat(ChatDTO dto) {
-        return chatModel.call(dto.message());
+//        return chatModel.call(dto.message());
+        return geminiChatModel.call(dto.message());
     }
 }
